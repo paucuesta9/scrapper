a4fc09f3f8f8793c8023f5cf07afba75fb6bc92a
Pau Cuesta <pau.cuesta@gmail.com>
2021-11-17 12:53:58 +0100

    Added iPad & SMS message

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const playwright = require('playwright-aws-lambda')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const serviceSid = process.env.TWILIO_SERVICE_SID
+const mobilePhone = process.env.MOBILE_PHONE
 
-const shops = [
-  // {
-  //   vendor: 'Amazon',
-  //   url: 'https://www.amazon.es/Cressi-Leonardo-Ordenador-Adultos-Unisex/dp/B01873N5YO',
-  //   desiredPrice: '120.00',
-  //   checkPrice: async ({ page, desiredPrice }) => {
-  //       //const pagina = await page.content()
-  //       //console.log(pagina)
-  //     //const content = await page.textContent('#newBuyBoxPrice')
-  //     //const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
-  //     //return desiredPrice >= actualPrice
-  //     return true
-  //   }
-  // },
+const client = require('twilio')(accountSid, authToken)
+
+const shopsLeonardo = [
   {
     vendor: 'Scubastore',
     url: 'https://www.scubastore.com/buceo-submarinismo/cressi-ordenador-leonardo/553284/p',
@@ -56,6 +49,135 @@ const shops = [
   }
 ]
 
+const shopsIPad = [
+  {
+    vendor: 'El Corte Inglés',
+    url: 'https://www.elcorteingles.es/electronica/A41127333-ipad-102-2021-64gb-wi-fi-gris-espacial/',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('.price')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'El Corte Inglés',
+    url: 'https://www.elcorteingles.es/electronica/A41127341-ipad-102-2021-64gb-wi-fi-plata/',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('.price')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'Fnac',
+    url: 'https://www.fnac.es/mp9085676/IPad-Apple-10-2-9-a-generacion-64GB-gris-espacial/',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('.f-priceBox-price')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'PcComponentes',
+    url: 'https://www.pccomponentes.com/apple-ipad-2021-102-64gb-wifi-cellular-gris-espacial',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('#precio-main')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'PcComponentes',
+    url: 'https://www.pccomponentes.com/apple-ipad-2021-102-64gb-wifi-cellular-plata',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('#precio-main')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'PcComponentes',
+    url: 'https://www.pccomponentes.com/apple-ipad-2021-102-64gb-wifi-gris-espacial',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('#precio-main')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  },
+  {
+    vendor: 'Carrefour',
+    url: 'https://www.carrefour.es/ipad-2591-cm-102-con-wifi-64gb-apple-gris-espacial/VC4A-15583813/p',
+    desiredPrice: '350.00',
+    checkPrice: async ({ page, desiredPrice }) => {
+      const content = await page.textContent('.buybox__price')
+      const actualPrice = parseFloat(content.replace('€', '').replace(',', '.').trim())
+      console.log('Actual price: ' + actualPrice)
+      return desiredPrice >= actualPrice
+    }
+  }
+]
+
+const checkProduct = async (context, shops, product) => {
+  const desired = []
+  for (const shop of shops) {
+    console.log('start')
+    const { checkPrice, vendor, url, desiredPrice } = shop
+    console.log('new page')
+    const page = await context.newPage()
+    console.log('goto ' + url)
+    await page.goto(url)
+    console.log('check')
+    const satisfies = await checkPrice({ page, desiredPrice })
+    if (satisfies) desired.push(vendor + ': ' + url)
+    console.log(`${vendor}: ${satisfies ? 'Product in desired price!' : 'Product not in desired price :\'('}`)
+    await page.close()
+  }
+
+  const desiredOn = desired.length > 0 ? `Disponible en: ${desired.join(', ')}` : 'Product not in desired price :\'('
+  console.log(desiredOn)
+
+  if (desired.length > 0) {
+    await sendEmail(product, desiredOn)
+    await sendSms(product, desiredOn)
+  }
+}
+
+const sendEmail = async (product, desiredOn) => {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: process.env.SEND_EMAIL_TO_CRESSI_LEONARDO,
+    from: process.env.SEND_EMAIL_FROM,
+    subject: product + ' ha baixat de preu!',
+    text: 'Resultats: ' + desiredOn,
+    html: '<p>Resultats: ' + desiredOn + '</p><b>Developed by Pau Cuesta</b>'
+  }
+  await sgMail.send(msg)
+}
+
+const sendSms = async (product, desiredOn) => {
+  client.messages
+    .create({
+      body: product + ' ha baixat de preu! Resultats: ' + desiredOn,
+      messagingServiceSid: serviceSid,
+      to: mobilePhone
+    })
+    .then(message => console.log(message.sid))
+    .catch(err => console.log(err))
+}
+
 exports.handler = async (event, context) => {
   const browser = null
 
@@ -63,36 +185,8 @@ exports.handler = async (event, context) => {
     const browser = await playwright.launchChromium()
     const context = await browser.newContext()
 
-    const desired = []
-    for (const shop of shops) {
-      console.log('start')
-      const { checkPrice, vendor, url, desiredPrice } = shop
-      console.log('new page')
-      const page = await context.newPage()
-      console.log('goto ' + url)
-      await page.goto(url)
-      console.log('check')
-      const satisfies = await checkPrice({ page, desiredPrice })
-      if (satisfies) desired.push(vendor + ': ' + url)
-      console.log(`${vendor}: ${satisfies ? 'Product in desired price!' : 'Product not in desired price :\'('}`)
-      await page.close()
-    }
-
-    const desiredOn = desired.length > 0 ? `Disponible en: ${desired.join(', ')}` : 'Product not in desired price :\'('
-    if (desired.length > 0) {
-      const sgMail = require('@sendgrid/mail')
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-      const msg = {
-        to: process.env.SEND_EMAIL_TO_CRESSI_LEONARDO,
-        from: process.env.SEND_EMAIL_FROM,
-        subject: 'Cressi Leonardo ha baixat de preu!',
-        text: 'Resultats: ' + desiredOn,
-        html: '<p>Resultats: ' + desiredOn + '</p><b>Developed by Pau Cuesta</b>'
-      }
-      await sgMail.send(msg)
-    }
-
-    console.log(desiredOn)
+    await checkProduct(context, shopsLeonardo, 'Cressi Leonardo')
+    await checkProduct(context, shopsIPad, 'iPad')
   } finally {
     if (browser) {
       await browser.close()
